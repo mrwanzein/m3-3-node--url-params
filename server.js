a6f5648195c7f697b03378c4798e1b6eeacb91a0
Mrwan Zein <mrwanzein@outlook.com>
2020-07-06 13:10:02 -0400

    Exercise 1.1 to 1.3 answer

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const morgan = require('morgan');
+const express = require('express');
 
 const { top50 } = require('./data/top50');
 
@@ -14,9 +15,16 @@ app.use(express.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
 
 // endpoints here
+app.get('/top50', (req, res) => {
+    res.status(404);
+    res.render('pages/top50', {
+        title: 'Top 50 Songs Streamed on Spotify',
+        top50: top50
+    });
+});
 
 // handle 404s
-app.git('*', (req, res) => {
+app.get('*', (req, res) => {
     res.status(404);
     res.render('pages/fourOhFour', {
         title: 'I got nothing',
@@ -24,4 +32,4 @@ app.git('*', (req, res) => {
     });
 });
 
-get.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
